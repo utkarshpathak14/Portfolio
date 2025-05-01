ab61be7be502835f4a661e5517e283448ad1d17b
Utkarsh Pathak <utkarshpathakji285@gmail.com>
2025-05-01 10:42:44 +0530

    fixed

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -9,7 +9,6 @@ import { Poppins } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 import { TracingBeam } from "./ui/tracing-beam";
 import Slider from "./Slider";
-import { ImagesSlider } from "./ui/images-slider";
 
 
 const poppins = Poppins({
@@ -25,9 +24,7 @@ function Work() {
         {dummyContent.map((item, index) => (
           <a href={item.link} key={`item-${index}`} target="_blank" rel="noopener noreferrer" className={poppins.className}>
           <div key={`content-${index}`} className="mb-10">
-            <h2 className="bg-black text-white font-bold rounded-full text-sm w-fit px-4 py-1 mb-4">
-              {item.badge}
-            </h2>
+            
 
             <p className={twMerge("text-xl mb-4")}>{item.title}</p>
 
--- src/components/ui/tracing-beam.tsx
+++ src/components/ui/tracing-beam.tsx
@@ -4,7 +4,7 @@ import {
   motion,
   useTransform,
   useScroll,
-  useVelocity,
+
   useSpring,
 } from "motion/react";
 import { cn } from "@/lib/utils";
